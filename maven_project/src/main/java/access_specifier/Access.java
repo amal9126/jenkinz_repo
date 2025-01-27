ee2b266840c732f844e7c557732b8e4c6294b29a
AMAL RAJ <AMAL RAJ@LAPTOP-92ICR69A>
2025-01-27 11:10:20 +0530

    sample commit

---
+++ maven_project/src/main/java/access_specifier/Access.java
@@ -0,0 +1,14 @@
+package access_specifier;
+
+public class Access {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		accessspecifier obj=new accessspecifier();
+		obj.print();
+	//	obj.display();  private access specifier accessible only within the class
+		obj.disp();
+		obj.prnt();
+	}
+
+}
---
+++ maven_project/src/main/java/aggregation/Address.java
@@ -0,0 +1,14 @@
+package aggregation;
+
+public class Address {
+
+public Address(String housename, String city, String state, int pincode) {
+		super();
+		this.housename = housename;
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+	}
+String housename,city,state;
+int pincode;
+}
